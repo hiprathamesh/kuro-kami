e5a809fc0cf98882c6d6d5f129a3ec4792340dd5
royalp <prathameshnikam21119@gmail.com>
2024-12-06 13:28:41 +0530

    change title

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,svelte,ts}', // Include Svelte and TypeScript files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
